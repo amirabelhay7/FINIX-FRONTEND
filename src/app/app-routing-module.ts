78df88ba059e1af9fe2395a0d9bb66ff722f899d
ghassen <ghassendhaoui.eng@gmail.com>
2026-02-09 15:23:33 +0100

    chore: reset architecture and cleanup templates for manual implementation

--- src/app/app-routing-module.ts
+++ src/app/app-routing-module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'front-office', pathMatch: 'full' },
+  // { path: 'front-office', component: FrontOfficeTemplateComponent },
+  // { path: 'back-office', component: BackOfficeTemplateComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
